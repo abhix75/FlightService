5f952d70f3467dfb5898898fddb5c36102a794b9
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-06-05 21:02:28 +0530

    Added new migration that can add a constraint in a column of Airport
    table
    
    [4mSequelize CLI [Node: 18.16.0, CLI: 6.6.0, ORM: 6.31.1][24m
    
    Successfully created migrations folder at "C:\Users\abhij\Developer\Flights\migrations".
    New migration was created at [94mC:\Users\abhij\Developer\Flights\migrations\20230605153228-update-city-airport-association.js[39m .

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async() => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+
+    // bad code alert
+    // const {Airport ,City } =require('./models');
+    // const Npd= await City.findByPk(1);
+    // console.log(Npd);
+    // // const Npdairport = await Npd.createAirport({name:'mikumegha Airport',code: 'MGH'});
+    // // console.log(Npdairport);
+    // const airportInNpd=await Npd.getAirports();
+    // console.log(airportInNpd)
 });
