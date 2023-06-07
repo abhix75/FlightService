17105c310044345eb0ea427101aa8c0aa77e9846
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-06-07 12:42:20 +0530

    forget to put foreign key in airplane model in previous one so updated it

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.Flight, {
+        foreignKey: 'airplaneId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   Airplane.init({
